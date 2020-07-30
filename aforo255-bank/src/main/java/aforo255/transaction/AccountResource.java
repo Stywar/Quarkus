21a76e25dbab4402abf5d817fd3ee07a63247e6f
Stywar <franco111988@gmail.com>
2020-07-30 03:06:10 -0500

    sea grego

--- aforo255-bank/src/main/java/aforo255/transaction/AccountResource.java
+++ aforo255-bank/src/main/java/aforo255/transaction/AccountResource.java
@@ -4,13 +4,43 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PathParam;
+import aforo255.transaction.domain.Account;
+import aforo255.transaction.services.IAccountService;
 
 @Path("/account")
 public class AccountResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
-    }
+	@Inject
+	IAccountService accountService;
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "hello";
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@RolesAllowed({"Admin"})
+	@Path("/listar")
+	public List<Account> listar() {
+
+		return accountService.findAllAccount();
+	}
+
+	@GET
+	@Path("/ver/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Account detalle(@PathParam("id") Integer id) {
+
+		Account account = accountService.findAccountById(id);
+		return account;
+	}
 }
